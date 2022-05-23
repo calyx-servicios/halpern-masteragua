7bb832080dfc7e081674171bfca2f72274314805
Fede Gregori <jfedericogregori@gmail.com>
2022-05-23 13:26:59 -0300

    [UPD][tango_reference_field] Migrated from 'tango_reference field'

--- tango_reference field/models/product_template.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError, ValidationError
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    tango_reference = fields.Char('Tango Reference', index=True, size=100)
